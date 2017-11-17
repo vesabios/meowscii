24b5b2a411f98abddc3390f2be7fb662d7477591
Steve Mason <vesabios@gmail.com>
2017-11-16 20:19:12 -0800

    asdf

---
+++ Assets/Projection.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projection : MonoBehaviour {
+
+    public Vector3 linePoint;
+    public Vector3 lineVec;
+    public Vector3 planePoint;
+    public Vector3 planeNormal;
+    public Vector3 result;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 intersection;
+
+        LinePlaneIntersection(out intersection, linePoint, lineVec, planeNormal, planePoint);
+
+        result = intersection;
+    }
+
+
+    //If the line and plane are not parallel, the function outputs true, otherwise false.
+    public static bool LinePlaneIntersection(out Vector3 intersection, Vector3 linePoint, Vector3 lineVec, Vector3 planeNormal, Vector3 planePoint)
+    {
+
+        float length;
+        float dotNumerator;
+        float dotDenominator;
+        Vector3 vector;
+        intersection = Vector3.zero;
+
+        //calculate the distance between the linePoint and the line-plane intersection point
+        dotNumerator = Vector3.Dot((planePoint - linePoint), planeNormal.normalized);
+        dotDenominator = Vector3.Dot(lineVec.normalized, planeNormal.normalized);
+
+        //line and plane are not parallel
+        if (dotDenominator != 0.0f)
+        {
+            length = dotNumerator / dotDenominator;
+
+            //create a vector from the linePoint to the intersection point
+            vector = lineVec.normalized * length;
+
+            //get the coordinates of the line-plane intersection point
+            intersection = linePoint + vector;
+
+            return true;
+        }
+
+        //output not valid
+        else
+        {
+            return false;
+        }
+    }
+
+}
